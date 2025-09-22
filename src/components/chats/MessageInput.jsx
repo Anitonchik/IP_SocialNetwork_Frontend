b0e8ece5255c2e1d003a14eb11812ef20cb9ec10
Anitonchik <anitonchikantonova@yandex.ru>
2025-09-22 21:31:46 +0400

    '

--- src/components/chats/MessageInput.jsx
+++ src/components/chats/MessageInput.jsx
@@ -1,4 +1,7 @@
 import "../../../styles.css"
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 import React, { useState } from "react";
 
 const MessageInput = ({ onSend }) => {
